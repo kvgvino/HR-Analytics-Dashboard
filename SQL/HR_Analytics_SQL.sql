e14675de3512899da5c35dfec4cd83becbbddf52
Vinoth G <kvgvino03@gmail.com>
2026-06-07 17:25:20 +0530

    Update HR_Analytics_SQL.sql

--- SQL/HR_Analytics_SQL.sql
+++ SQL/HR_Analytics_SQL.sql
@@ -1,1 +1,27 @@
+-- Total Employees
+SELECT COUNT(*) AS Total_Employees
+FROM employees;
 
+-- Attrition Count
+SELECT Attrition,
+COUNT(*) AS Employee_Count
+FROM employees
+GROUP BY Attrition;
+
+-- Department Wise Employees
+SELECT Department,
+COUNT(*) AS Employee_Count
+FROM employees
+GROUP BY Department;
+
+-- Average Salary by Department
+SELECT Department,
+ROUND(AVG(MonthlyIncome),2) AS Avg_Salary
+FROM employees
+GROUP BY Department;
+
+-- Gender Distribution
+SELECT Gender,
+COUNT(*) AS Employee_Count
+FROM employees
+GROUP BY Gender;
